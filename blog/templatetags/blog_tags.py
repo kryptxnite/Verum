103d97a18f8ffd2624f993f35841fb174bdf21fc
kryptxnite <chiefyuldashev@gmail.com>
2026-02-19 00:27:07 +0900

    Markdown markup for posts has been created.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,8 +1,17 @@
 from django import template
 from blog.models import Category
+from django.utils.safestring import mark_safe
+
+import markdown
 
 register = template.Library()
+
 
 @register.simple_tag
 def get_all_categories():
     return Category.objects.all()
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
