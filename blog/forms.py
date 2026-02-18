ef8eef39a9d786916c415b5ef93bd596763b7079
kryptxnite <chiefyuldashev@gmail.com>
2026-02-18 20:20:12 +0900

    Cosmetic changes on comments section.

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,21 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'full-width',
+                'placeholder': 'Your Name',
+                'name': 'cName'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'full-width',
+                'placeholder': 'Your Email',
+                'name': 'cEmail'
+            }),
+            'body': forms.Textarea(attrs={
+                'class': 'full-width',
+                'placeholder': 'Your Comment',
+                'name': 'cMessage'
+            }),
+        }
