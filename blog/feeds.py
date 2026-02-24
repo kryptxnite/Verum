52d0b3021dab4bd6e834c950ea1ca887e3056202
kryptxnite <chiefyuldashev@gmail.com>
2026-02-24 16:54:17 +0900

    The rss feed has been created and configured.

---
+++ blog/feeds.py
@@ -0,0 +1,22 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords_html
+from django.urls import reverse_lazy
+from .models import Post
+import markdown
+
+class LatestPostsFeed(Feed):
+    title = 'Verum.'
+    link = reverse_lazy('blog:post_list')
+    description = 'Latest news from Verum.'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords_html(markdown.markdown(item.body), 30)
+
+    def item_pubdate(self, item):
+        return item.publish
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.posts, name='post_category_list'),
     path('post/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-    path('tag/<slug:tag_slug>/', views.posts, name='post_list_by_tag')
+    path('tag/<slug:tag_slug>/', views.posts, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
 
