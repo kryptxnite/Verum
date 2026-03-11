41145df53b1663df8ef28edbeef85b7b58a4f16d
kryptxnite <chiefyuldashev@gmail.com>
2026-03-11 10:01:52 +0900

    Improving the search engine.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.postgres.search import SearchVector
 from .forms import CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
 
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline
+)
+
+
+
+# Post section
+# ========================
 def posts(request, category_slug=None, tag_slug=None):
+
     posts = Post.published.all()
     category = None
     tag = None
@@ -20,25 +31,37 @@ def posts(request, category_slug=None, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts = posts.filter(tags__in=[tag])
 
-    paginator = Paginator(posts, 9)
+    paginator = Paginator(posts, 12)
     page_number = request.GET.get('page', 1)
+    
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts,
-                   'category': category,
-                   'tag': tag,})
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,
+            'category': category,
+            'tag': tag,
+        }
+    )
 
 
+
+# Post detail section
+# ========================
 def post_detail(request, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post)
+
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post)
+        
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
@@ -54,38 +77,70 @@ def post_detail(request, post):
                     'similar_posts': similar_posts})
 
 
+
+# Comment section
+# ========================
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(Post,
-                            id=post_id,
-                            status=Post.Status.PUBLISHED)
+
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED)
+        
     comment = None
     form = CommentForm(data=request.POST)
+
     if form.is_valid:
         comment = form.save(commit=False)
         comment.post = post
         comment.save()
-    return render(request, 'blog/post/comment.html',
-                            {'post': post,
-                            'form': form,
-                            'comment': comment})
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
+
 
 
 def post_search(request):
-    form = SearchForm()
+
+    form = SearchForm(request.GET or None)
     query = None
     results = []
 
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+    if form.is_valid():
 
-    return render(request,
-                'blog/post/search.html',
-                {'form': form,
-                'query': query,
-                'results': results})
+        query = form.cleaned_data['query']    
+
+        search_vector = (
+            SearchVector('title', weight='A') +
+            SearchVector('body', weight='B')
+        )
+
+        search_query = SearchQuery(query)
+
+        results = Post.published.annotate(
+            rank = SearchRank(search_vector, search_query),
+            headline = SearchHeadline(
+                'body',
+                search_query,
+                start_sel = '<mark>',
+                stop_sel = '</mark>'
+            )
+        ).filter(rank__gte=0.1).order_by('-rank')
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
+    )
